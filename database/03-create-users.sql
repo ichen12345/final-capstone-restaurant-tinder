d49ffc91019ea97078ac90ffdc813793a7e4a491
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2022-08-15 08:04:04 -0600

    updated db usernames

--- database/03-create-users.sql
+++ database/03-create-users.sql
@@ -2,7 +2,7 @@
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
 
-CREATE USER brewery_finder_owner WITH PASSWORD 'restaurant_tinder_owner1';
+CREATE USER restaurant_tinder_owner WITH PASSWORD 'restaurant_tinder_owner1';
 
 GRANT ALL 
 ON ALL TABLES IN SCHEMA public
@@ -12,7 +12,7 @@ GRANT ALL
 ON ALL SEQUENCES IN SCHEMA public
 TO restaurant_tinder_owner;
 
-CREATE USER brewery_finder_appuser WITH PASSWORD 'restaurant_tinder_appuser1';
+CREATE USER restaurant_tinder_appuser WITH PASSWORD 'restaurant_tinder_appuser1';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
