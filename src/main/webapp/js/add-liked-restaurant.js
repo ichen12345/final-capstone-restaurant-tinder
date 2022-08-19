5c3fbec8e54291d0394873d658668f2b127d14fe
John Fulginiti <jfulginiti4@gmail.com>
2022-08-19 15:56:17 -0500

    began work on add js function for calling the RestaurantController on Like click

---
+++ src/main/webapp/js/add-liked-restaurant.js
@@ -0,0 +1,28 @@
+async function addLikedRestaurant(card) {
+    const restaurantId = $("#");
+    const restaurantName = $("#");
+    const imageURL = $("#");
+    const price = $("#").length; //price is return by yelp api as a string of $
+    const rating = $("#");
+    const address = $("#");
+
+
+
+    const url = "/likedRestaurants";
+    const csrf_token = $('meta[name="csrf-token"]').attr('content');
+
+    const body = {
+        'CSRF_TOKEN' : csrf_token,
+        'restaurant_id': restaurantId,
+        'restaurant_name': restaurantName,
+        'image_url': imageURL,
+        'price': price,
+        'rating': rating,
+        'address': address
+    };
+
+
+    $.get(url, body)
+
+
+}
